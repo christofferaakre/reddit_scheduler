9c125d9bb8434a36968ed0217f16a7bc8ec40dfc
Christoffer Aakre <christoffer.corfield@gmail.com>
2022-04-26 00:32:30 +0200

    getting ready to handle form

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def main():
     def submission_form():
         return app.send_static_file('submit_post.html')
 
+    @app.route("/schedule_post")
+    def process_submission():
+        pass
+
     app.run(debug=True)
 
 if __name__ == "__main__":
