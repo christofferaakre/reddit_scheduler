310065e525d3560831d18fd2b60a5ecbad7bc642
Christoffer Aakre <christoffer.corfield@gmail.com>
2022-04-26 21:22:09 +0200

    integer dates

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import json
 import os
 from path import Path
 
+from posts import schedule_post
+
 def main():
     app = Flask(__name__)
 
@@ -17,15 +19,18 @@ def main():
     @app.route("/schedule_post", methods=["POST", "GET"])
     def process_submission():
         form_data = request.form.to_dict()
-        print(form_data)
+        form_data['date'] = int(form_data['date'])
         filename = f'{uuid.uuid4()}.json'
-
-        with open(posts_directory / filename, 'w') as file:
+        save_path = posts_directory / filename
+        with open(save_path, 'w') as file:
             json_string = json.dumps(form_data, indent=2)
             file.write(json_string)
 
 
         print(f'filename: {filename}')
+
+        command, time = schedule_post(save_path)
+
         return redirect('/')
 
     app.run(debug=True)
